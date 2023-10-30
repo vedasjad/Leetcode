44473a6174941841f207c5fcc2c73b68b0bfe7f1
Asjad <thenotsosociableguy@gmail.com>
2023-10-31 00:11:41 +0530

    Time: 5 ms (82.42%) | Memory: 7.8 MB (15.68%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,19 @@
 class Solution
 {
     public:
-        bool isAnagram(string s, string t) {
-            if(s.length()!=t.length()) return false;
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-            for(int i=0;i<s.length();i++){
-                if(s[i]!=t[i]) return false;
+        bool isAnagram(string s, string t)
+        {
+            if (s.length() != t.length()) return false;
+            unordered_map<int, int> imap1, imap2;
+            for (int i = 0; i < s.length(); i++)
+            {
+                imap1[s[i]]++;
+                imap2[t[i]]++;
+            }
+            if (imap1.size() != imap2.size()) return false;
+            for (auto it: imap1)
+            {
+                if (it.second != imap2[it.first]) return false;
             }
             return true;
         }
