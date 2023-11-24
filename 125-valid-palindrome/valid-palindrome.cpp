74aab92134d9f5f68432553e69b34b384ff04376
Asjad <thenotsosociableguy@gmail.com>
2023-11-24 18:43:43 +0530

    Time: 8 ms (34.77%) | Memory: 7.7 MB (47.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,56 @@
 class Solution
 {
-    public:
-        bool isPalindrome(string s)
+    private:
+        bool checkPalindrome(int left, int right)
         {
-            int i=0,j=s.length()-1;
-            bool flag=true;
-            while(i<=j){
-                char first = tolower(s[i]),last = tolower(s[j]);
-                if(first<'0' || first>'9' && first<'a' || first>'z') i++;
-                else if(last<'0' || last>'9' && last<'a' || last>'z') j--;
-                else{
-                    if(first!=last){
-                        flag=false;
-                        break;
+            char leftChar = tolower(s[left]), rightChar = tolower(s[right]);
+            if (leftChar >= '0' && leftChar <= '9' || leftChar >= 'a' && leftChar <= 'z')
+            {
+                if (rightChar >= '0' && rightChar <= '9' || rightChar >= 'a' && rightChar <= 'z')
+                {
+                    if (leftChar == rightChar)
+                    {
+                        if (left + 1 < right - 1)
+                        {
+                            return checkPalindrome(left + 1, right - 1);
+                        }
                     }
-                    j--;
-                    i++;
+                    else
+                    {
+                        return false;
+                    }
                 }
-            }   
-            return flag;
+                else
+                {
+                    if (left < right - 1)
+                    {
+                        return checkPalindrome(left, right - 1);
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                if (left + 1 < right)
+                {
+                    return checkPalindrome(left + 1, right);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
         }
+
+    public:
+        string s;
+    bool isPalindrome(string str)
+    {
+        s = str;
+        return checkPalindrome(0, s.length() - 1);
+    }
 };
