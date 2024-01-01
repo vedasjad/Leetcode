879a589b53996b63c2fe6d2b06ccf5f7fdddace5
Asjad <thenotsosociableguy@gmail.com>
2024-01-01 15:07:03 +0530

    Time: 46 ms (8.33%) | Memory: 17.8 MB (36.11%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -3,14 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
-        int j=0,count=0;
+        int j=0;
         for(int i=0;i<s.size();i++){
-            if(j>=g.size()) return count;
+            if(j>=g.size()) return j;
             if(g[j]<= s[i]){
                 j++;
-                count++;
             }
         }
-        return count;
+        return j;
     }
 };
