c4f518dc2acb00842b3719c90d230f9843dfa2ae
Asjad <thenotsosociableguy@gmail.com>
2024-04-16 21:00:26 +0530

    Time: 19 ms (9.46%) | Memory: 24.9 MB (14.90%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.cpp
+++ 623-add-one-row-to-tree/add-one-row-to-tree.cpp
@@ -6,32 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* addOneRow(TreeNode* root, int val, int depth,int currentDepth =1) {
-        if(depth==1 || !root){
+    TreeNode* addOneRow(TreeNode* root, int val, int depth,
+                        int currentDepth = 1) {
+        if (depth == 1 || !root) {
             TreeNode* newNode = new TreeNode(val);
-            newNode->left=root;
+            newNode->left = root;
             return newNode;
         }
-        if(depth-1==currentDepth){
-            // if(root->left){
-                TreeNode* newNode1 = new TreeNode(val);
-                newNode1->left=root->left;
-                root->left=newNode1;
-            // }
-            // if(root->right){
-                TreeNode* newNode2 = new TreeNode(val);
-                newNode2->right=root->right;
-                root->right=newNode2;
-            // }
+        if (depth - 1 == currentDepth) {
+            TreeNode *newNode1 = new TreeNode(val),
+                     *newNode2 = new TreeNode(val);
+            newNode1->left = root->left;
+            root->left = newNode1;
+            newNode2->right = root->right;
+            root->right = newNode2;
             return root;
         }
-        addOneRow(root->left,val,depth,currentDepth+1);
-        addOneRow(root->right,val,depth,currentDepth+1);
+        addOneRow(root->left, val, depth, currentDepth + 1);
+        addOneRow(root->right, val, depth, currentDepth + 1);
         return root;
     }
 };
