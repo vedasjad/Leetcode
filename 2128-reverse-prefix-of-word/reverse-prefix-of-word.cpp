4f255338383c837fc811d05e037586dd9dc4ccba
Asjad <thenotsosociableguy@gmail.com>
2024-05-01 19:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (91.39%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i = 0, j = 0;
+        while (j < word.size() && word[j] != ch)
+            j++;
+        if (j == word.size())
+            return word;
+        while (i < j)
+            swap(word[i++], word[j--]);
+        return word;
+    }
+};
