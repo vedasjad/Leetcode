54a76ec2dea13065d115fd87769ea102ca9f919c
Asjad <thenotsosociableguy@gmail.com>
2024-05-06 12:33:15 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (96.54%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int arr[39]= {0,1,1};
+        int arr[4]= {0,1,1};
         for(int i=3;i<=n;i++){
-            arr[i] = arr[i-1]+arr[i-2]+arr[i-3];
+            arr[i%4] = arr[(i-1)%4]+arr[(i-2)%4]+arr[(i-3)%4];
         }
-        return arr[n];
+        return arr[n%4];
     }
-
 };
