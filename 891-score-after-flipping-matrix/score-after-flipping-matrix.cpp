57b346fea2b3e2fd496ab3a2825a189eedca3283
Asjad <thenotsosociableguy@gmail.com>
2024-05-14 00:05:47 +0530

    Time: 3 ms (41.73%) | Memory: 10.5 MB (9.21%) - LeetSync

--- 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-        unordered_set<int> irow,icol;
-        int m=grid.size(),n=grid[0].size();
-        for(int i=0;i<m;i++){
-            if(!grid[i][0]) irow.insert(i);
+        unordered_set<int> irow, icol;
+        int m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; i++) {
+            if (!grid[i][0])
+                irow.insert(i);
         }
-        int count=0;
-        for(int i=1;i<n;i++){
-            count=0;
-            for(int j=0;j<m;j++){
-                if(irow.count(j) && !grid[j][i]){
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            count = 0;
+            for (int j = 0; j < m; j++) {
+                if (irow.count(j) && !grid[j][i]) {
                     count++;
                 }
-                if(!irow.count(j) && grid[j][i]){
+                if (!irow.count(j) && grid[j][i]) {
                     count++;
                 }
             }
-            if(count<=m/2){
+            if (count <= m / 2) {
                 icol.insert(i);
             }
         }
@@ -29,22 +30,23 @@ public:
         //     cout<<it<<" ";
         // }
         // cout<<endl;
-        int total=0,row=0;
-        for(int i=0;i<m;i++){
-            row=0;
-            for(int j=0;j<n;j++){
-                if(irow.count(i) && icol.count(j)){
-                    if(grid[i][j]) row+=pow(2,n-1-j);
-                }
-                else if(irow.count(i) ^ icol.count(j)){
-                    if(!grid[i][j]) row+=pow(2,n-1-j);
-                }
-                else if(!irow.count(i) && !icol.count(j)){
-                    if(grid[i][j]) row+=pow(2,n-1-j);
+        int total = 0, row = 0;
+        for (int i = 0; i < m; i++) {
+            row = 0;
+            for (int j = 0; j < n; j++) {
+                if (irow.count(i) && icol.count(j)) {
+                    if (grid[i][j])
+                        row += pow(2, n - 1 - j);
+                } else if (irow.count(i) ^ icol.count(j)) {
+                    if (!grid[i][j])
+                        row += pow(2, n - 1 - j);
+                } else if (!irow.count(i) && !icol.count(j)) {
+                    if (grid[i][j])
+                        row += pow(2, n - 1 - j);
                 }
             }
             // cout<<row<<endl;
-            total+=row;
+            total += row;
         }
         return total;
     }
