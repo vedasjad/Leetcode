92a6084b3770abb1253710d58cc60fc3216135f0
Asjad <thenotsosociableguy@gmail.com>
2024-05-14 12:08:03 +0530

    Time: 1011 ms (32.68%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,7 +2,6 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 SET N = N-1;
     RETURN (
-        SELECT DISTINCT(salary) from Employee order by salary DESC
-        LIMIT 1 OFFSET N
+        SELECT DISTINCT(salary) FROM Employee ORDER BY salary DESC LIMIT 1 OFFSET N
     );
 END
